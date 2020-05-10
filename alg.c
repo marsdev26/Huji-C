158ebadd58129e57014d50e48006ac231960e37b
M. Marciano <49877926+marsdev26@users.noreply.github.com>
2020-05-10 21:26:58 +0300

    Update alg.c

--- alg.c
+++ alg.c
@@ -2,7 +2,7 @@ int algorithm(int ** a, RoadParts *parts, char algInParts[MAX_LENGTH_LINE],
               long numRoadLength, int numRoadParts, long numInitialParts)
 {
     int min = INT_MAX;
-    int min_price;
+    int min_price = 0;
     int temp;
 
     assignIndex(parts, numRoadParts, algInParts, numInitialParts);
@@ -24,22 +24,7 @@ int algorithm(int ** a, RoadParts *parts, char algInParts[MAX_LENGTH_LINE],
                 a[i][k] = 0;
                 continue;
             }
-            for (int h = 0; h < numRoadParts; h++)
-            {
-
-                if (parts[h].rightPart != algInParts[k] || (i - parts[h].lengthPart) < 0 || a[i -
-                                                                                              parts[h].lengthPart][parts[h].index] == INT_MAX)
-                {
-                    continue;
-                }
-
-                min_price = parts[h].pricePart + a[i-parts[h].lengthPart][parts[h].index];
-
-                if (min_price < min)
-                {
-                    min = min_price;
-                }
-            }
+            min = findMin(numRoadParts, a, parts, k, i, algInParts, min_price, min);
             a[i][k] = min;
             min = INT_MAX;
         }
@@ -48,15 +33,15 @@ int algorithm(int ** a, RoadParts *parts, char algInParts[MAX_LENGTH_LINE],
 
     for(int l = 0; l < (int)numInitialParts; l++)
     {
-        temp = a[(int) numRoadLength][l];
-        if (temp < min)
-        {
-            min = temp;
-        }
-        else
-        {
-            continue;
-        }
+            temp = a[(int) numRoadLength][l];
+            if (temp < min)
+            {
+                min = temp;
+            }
+            else
+            {
+                continue;
+            }
     }
 
     if (min == INT_MAX)
