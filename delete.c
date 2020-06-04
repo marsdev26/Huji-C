41fe63c8eeee38c345d2dca5da7962199eb5d3c6
M. Marciano <49877926+marsdev26@users.noreply.github.com>
2020-06-05 02:05:37 +0300

    Create delete.c

---
+++ delete.c
@@ -0,0 +1,54 @@
+void concreteDeletionParentNonNULLCase(RBTree *tree, Node** nodeToDelete, Node* nodeOfChild)
+{
+    if ((*nodeToDelete)->parent->right == *nodeToDelete)
+    {
+        (*nodeToDelete)->parent->right = nodeOfChild;
+        if (nodeOfChild != NULL)
+        {
+            nodeOfChild->parent = (*nodeToDelete)->parent;
+        }
+        tree->freeFunc((*nodeToDelete)->data);
+        free(*nodeToDelete);
+        *nodeToDelete = NULL;
+    }
+    else
+    {
+        (*nodeToDelete)->parent->left = nodeOfChild;
+        if (nodeOfChild != NULL)
+        {
+            nodeOfChild->parent = (*nodeToDelete)->parent;
+        }
+        tree->freeFunc((*nodeToDelete)->data);
+        free(*nodeToDelete);
+        *nodeToDelete = NULL;
+    }
+    /**
+    tree->freeFunc((*nodeToDelete)->data);
+    free(*nodeToDelete);
+    *nodeToDelete = NULL;
+     */
+}
+
+void concreteDeletionParentNULLCase(RBTree *tree, Node** nodeToDelete, Node* nodeOfChild)
+{
+    tree->freeFunc(tree->root->data);
+    free(tree->root);
+
+    if (nodeOfChild != NULL)
+    {
+        nodeOfChild->parent = NULL;
+    }
+    tree->root = nodeOfChild;
+}
+
+void concreteDeletionCases(RBTree *tree, Node** nodeToDelete, Node* nodeOfChild)
+{
+    if ((*nodeToDelete)->parent == NULL)
+    {
+        concreteDeletionParentNULLCase(tree, nodeToDelete, nodeOfChild);
+    }
+    else
+    {
+        concreteDeletionParentNonNULLCase(tree, nodeToDelete, nodeOfChild);
+    }
+}
